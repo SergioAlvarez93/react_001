8c6078d5c1eb4585c57f94c2c6840bf99dafe492
SergioAlvarez93 <EstebanSolari@yandex.ru>
2025-08-31 18:38:35 +0400

    commit_5

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,29 @@
 import React from "react";
+import restaurants from "../sample-restaurants";
 
 class Landing extends React.Component {
   render() {
-    return <p>Hello App!</p>;
+    return (
+      <div className="restaurant_select">
+        <div className="restaurant_select_top">
+          <div className="restaurant_select_top-header font-effect-outline">
+            Выбери ресторан
+          </div>
+          <div className="arrow_picker">
+            <div className="arrow_picker-up"></div>
+            <div className="arrow_picker-down"></div>
+          </div>
+        </div>
+        <div className="restaurant_select_bottom">
+          <ul>
+            {restaurants.map((e) => {
+              return <li key={e.id}>{e.title}</li>;
+            })}
+          </ul>
+        </div>
+        <button>Перейти в ресторан</button>
+      </div>
+    );
   }
 }
 
